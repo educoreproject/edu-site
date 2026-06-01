69a4be408c4121ef6823efdbb48b57f314b4015a
Mark Zebley <mzebley@aemcorp.com>
2026-06-01 18:32:37 -0400

    feat: implement DSU home page

---
+++ src/routes/dsu/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import DsuHomePage from '../+page.svelte';
+	import type { DsuHomePage as DsuHomePageContent, SiteChrome } from '$lib/content/types';
+
+	type Props = {
+		data: {
+			page: DsuHomePageContent;
+			chrome: SiteChrome;
+		};
+	};
+
+	let { data }: Props = $props();
+</script>
+
+<DsuHomePage {data} />
